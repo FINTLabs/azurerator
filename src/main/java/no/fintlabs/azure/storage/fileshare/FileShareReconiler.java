779efd50082d753907c9cdb53c41d705971f8252
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2022-11-10 20:28:30 +0100

    FLAIS-24 #comment Updated starter dep

--- src/main/java/no/fintlabs/azure/storage/fileshare/FileShareReconiler.java
+++ src/main/java/no/fintlabs/azure/storage/fileshare/FileShareReconiler.java
@@ -2,6 +2,7 @@ package no.fintlabs.azure.storage.fileshare;
 
 import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
 import io.javaoperatorsdk.operator.api.reconciler.dependent.Deleter;
+import io.javaoperatorsdk.operator.api.reconciler.dependent.DependentResource;
 import io.javaoperatorsdk.operator.api.reconciler.dependent.EventSourceProvider;
 import lombok.extern.slf4j.Slf4j;
 import no.fintlabs.FlaisReconiler;
@@ -17,7 +18,7 @@ import java.util.List;
 public class FileShareReconiler extends FlaisReconiler<FileShareCrd, FileShareSpec> {
 
     public FileShareReconiler(FileShareWorkflow workflow,
-                              List<? extends EventSourceProvider<FileShareCrd>> eventSourceProviders,
+                              List<? extends DependentResource<?, FileShareCrd>> eventSourceProviders,
                               List<? extends Deleter<FileShareCrd>> deleters) {
         super(workflow, eventSourceProviders, deleters);
     }
